5ddbfd211bdf6c474be9ca5c8910b306fc5090b4
Adeonir Kohl <adeonir@gmail.com>
2019-08-01 09:15:27 -0300

    Add user update method

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -7,6 +7,7 @@ class UserController {
     if (userExists) {
       return res.status(400).json({ error: 'User already exists!' })
     }
+
     const { id, name, email, provider } = await User.create(req.body)
 
     return res.json({
@@ -18,9 +19,29 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId)
+    const { email, oldPassword } = req.body
+    const user = await User.findByPk(req.userId)
 
-    return res.json({ ok: true })
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } })
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists!' })
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match!' })
+    }
+
+    const { id, name, provider } = await user.update(req.body)
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    })
   }
 }
 
